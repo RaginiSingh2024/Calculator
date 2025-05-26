4b55bfda8baacc3b9619c7d3b667d6068155fca1
Ragini Singh <raginisingh.sejal@gmail.com>
2025-05-26 17:03:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function appendValue(val) {
-    document.getElementById("display").value += val;
+ function appendValue(val) {
+  document.getElementById("display").value += val;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function backspace() {
+  let display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (err) {
+    document.getElementById("display").value = "Error";
   }
-  
-  function clearDisplay() {
-    document.getElementById("display").value = "";
-  }
-  
-  function backspace() {
-    let display = document.getElementById("display");
-    display.value = display.value.slice(0, -1);
-  }
-  
-  function calculate() {
-    try {
-      let result = eval(document.getElementById("display").value);
-      document.getElementById("display").value = result;
-    } catch (err) {
-      document.getElementById("display").value = "Error";
-    }
-  }
+}
+
+
+const toggleBtn = document.getElementById("mode");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
